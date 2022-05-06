422b3079cae586e8c6fced089b90090e2e247a07
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-05-06 19:49:03 +0300

    add 34th project

---
+++ day-34-quizzler-app-gui/data.py
@@ -0,0 +1,11 @@
+import requests
+
+PARAMETERS = {
+    "amount": 10,
+    "type": "boolean",
+    "category": 18
+}
+
+response = requests.get("https://opentdb.com/api.php", params=PARAMETERS)
+data = response.json()
+question_data = data["results"]
